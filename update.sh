214f2955a8abaac554e98a4da6193a2c667ff47c
Niclas Öhman <nikke@headdown.net>
2025-06-16 18:59:22 +0200

    Update.sh updated

--- update.sh
+++ update.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-source ~/cigarr-venv/bin/activate
+
+# Gå till projektkatalog
+cd /home/nikke/cigarrprojekt || exit 1
+
+# Aktivera venv
+source /home/nikke/cigarr-venv/bin/activate
+
+# Kör skripten
+echo "🕒 $(date --iso-8601=seconds)"
 python3 log_cigarr.py
 python3 generate_statistics.py
 python3 generate_stats_json.py
 python3 generate_html.py
-git add docs/index.html cigarrdata.db
-git commit -m "Auto update $(date +'%Y-%m-%d %H:%M:%S')"
-git push
 
+# Lägg till, committa och pusha
+git add docs/index.html docs/stats.html docs/stats_data.json cigarrdata.db
+git commit -m "Auto update $(date --iso-8601=seconds)"
+git push
